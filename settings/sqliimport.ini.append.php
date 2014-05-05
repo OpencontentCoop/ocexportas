9f9e2e88965243f08db1fc20053043c1d9f57c3f
Luca Realdi <luca.realdi@opencontent.it>
2014-05-05 15:47:19 +0200

    Add xml sqliimporter

---
+++ settings/sqliimport.ini.append.php
@@ -0,0 +1,14 @@
+<?php /* #?ini charset="utf-8"?
+
+[ImportSettings]
+AvailableSourceHandlers[]=importasxml
+
+[importasxml-HandlerSettings]
+Enabled=true
+Name=ImportAsXML Handler
+ClassName=ImportAsXMLImportHandler
+Debug=enabled
+DefaultParentNodeID=2
+
+
+*/ ?>
